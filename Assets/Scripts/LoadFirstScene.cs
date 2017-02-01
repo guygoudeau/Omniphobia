8197004603d5b572cf924c918c1d806e6c473caa
Austin Morrell <austinlm19@gmail.com>
2017-01-31 18:02:52 -0600

    first commit
    Testing code

---
+++ Assets/Scripts/LoadFirstScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadFirstScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        SceneChanger.LoadFirstScene(1);
+	}
+}
---
+++ Assets/Scripts/stuff.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stuff : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown("x"))
+        {
+            SceneChanger.ChangeScene(Player, 2);
+        }
+	}
+}
