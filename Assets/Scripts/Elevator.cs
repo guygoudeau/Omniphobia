a4a8494212a0c205a8912e856e40ee5c6df8dc06
Austin Morrell <austinlm19@gmail.com>
2017-02-06 13:45:34 -0600

    Start Elevator

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+    public Vector3 Destination;
+    public float speed;
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Lerp());
+    }
+
+    private IEnumerator Lerp()
+    {
+        while (gameObject.transform.position != Destination)
+        {
+            Vector3 temp;
+            temp = Destination * Time.deltaTime;
+            temp *= speed;
+            gameObject.transform.position = transform.position + temp;
+            yield return 0;
+        }
+    }
+}
