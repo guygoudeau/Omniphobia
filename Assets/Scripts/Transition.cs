694be43de6831bf7006e620b546c69d997c02da7
Austin Morrell <austinlm19@gmail.com>
2017-02-07 17:50:50 -0600

    A few scripts

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transition : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 newPosition;
+    public int SceneNumber;
+
+	void OnTriggerEnter(Collider obj)
+    {
+        player.transform.position = newPosition;
+        SceneChanger.ChangeScene(player, SceneNumber);
+    }
+}
