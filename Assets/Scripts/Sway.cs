3ebcbe48e9a3d83418d7bb36e7daabacac0810db
Rory Vaughn <Rvaughn2525@gmail.com>
2017-02-16 15:51:27 -0600

    end of thursday

--- Assets/Scripts/Sway.cs
+++ Assets/Scripts/Sway.cs
@@ -17,10 +17,10 @@ public class Sway : MonoBehaviour {
 
         Quaternion Q = startPos;
         //Vector3 V = transform.position;
-        Q.w += Delta * Mathf.Sin(Time.fixedTime * speed);
-        Q.x += Delta  * Mathf.Sin(Time.fixedTime * speed);
+        Q.w += Delta * Mathf.Cos(Time.fixedTime * speed);
+        Q.x += Delta  * Mathf.Cos(Time.fixedTime * speed);
         Q.z += Delta  * Mathf.Cos(Time.fixedTime * speed);
-        Q.y += Delta * Mathf.Sin(Time.fixedTime * speed);
+        Q.y += Delta * Mathf.Cos(Time.fixedTime * speed);
         //V.y += (Delta * Mathf.Sin(Time.fixedTime * speed));
         //transform.position = V;
         transform.rotation = Q;
