4f5770280ded8cf81f53eb95aeacbad976187032
Rory Vaughn <Rvaughn2525@gmail.com>
2017-02-21 14:25:06 -0600

    Glass break from walking

---
+++ Assets/Scripts/Glass.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glass : MonoBehaviour {
+
+    public GameObject GlassPrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
