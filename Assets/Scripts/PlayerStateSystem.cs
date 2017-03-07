f947bef0adf67164ad8d1d080ecf36b87ec0d2e5
Rory Vaughn <Rvaughn2525@gmail.com>
2017-03-07 14:30:39 -0600

    Scriptable

---
+++ Assets/Scripts/PlayerStateSystem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class EventPlayerDeath : UnityEvent
+{
+
+}
+public class PlayerStateSystem : MonoBehaviour {
+    public static EventPlayerDeath OnPlayerDeath;
+
+    private void Awake()
+    {
+
+        OnPlayerDeath = new EventPlayerDeath();
+    }
+
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            OnPlayerDeath.Invoke();
+
+        }
+        if (transform.position.y <= -200)
+        {
+            OnPlayerDeath.Invoke();
+        }
+        
+
+}
+}
---
+++ Assets/Scripts/ScriptableFearList.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+[CreateAssetMenu(fileName = "FearList", menuName = "Fear/Fear List", order = 1)]
+public class ScriptableFearList : ScriptableObject
+{
+    public List<ScriptableFear> ScriptableFears;
+    
+    public void Create(List<Fear> fears)
+    {
+        ScriptableFears = new List<ScriptableFear>();
+        
+        foreach(var f in fears)
+        {
+            ScriptableFear sf = CreateInstance<ScriptableFear>();
+            sf.Create(f);
+            ScriptableFears.Add(sf);
+        }
+        
+        
+    }
+}
