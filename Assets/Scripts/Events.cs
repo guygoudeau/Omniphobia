67d952c8987a661335aca9c9de00fc0e2e669f44
Guy Goudeau <guy.goudeau@students.aie.edu.au>
2017-03-07 18:16:08 -0600

    demo build done

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Events;
+
+public static class Events
+{
+    public static PlayerEvent PlayerWin = new PlayerEvent();
+    public static PlayerEvent PlayerDeath = new PlayerEvent();
+    public static PlayerEvent PlayerReloadScene = new PlayerEvent();
+    public static PlayerEvent PlayerForceScene = new PlayerEvent();
+}
+
+public class PlayerEvent : UnityEvent
+{
+}
