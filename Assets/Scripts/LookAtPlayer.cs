7f2e692ebd488057670741bea884f3d234ff76b4
Logan Pletcher <lpletch95@hotmail.com>
2017-03-14 12:57:47 -0500

    Started working on the Dolls scene and a new script

---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour {
+
+    private GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+        _player = GameObject.Find("OVRPlayerController");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(this.gameObject.GetComponent<MeshRenderer>().isVisible)
+        {
+            //Quaternion targetRot = Quaternion.Euler(_player.)
+            transform.LookAt(_player.transform);
+        }
+
+	}
+}
