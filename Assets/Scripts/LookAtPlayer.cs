e89f09a9f8c0555d9dc8db196476d36eddee347a
Logan Pletcher <lpletch95@hotmail.com>
2017-03-14 14:15:10 -0500

    Completed LookAtPlayer script(?)

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class LookAtPlayer : MonoBehaviour {
 
     private GameObject _player;
+    public bool IsVisible = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +15,16 @@ public class LookAtPlayer : MonoBehaviour {
 	void Update () {
         if(this.gameObject.GetComponent<MeshRenderer>().isVisible)
         {
-            //Quaternion targetRot = Quaternion.Euler(_player.)
+            Quaternion PreviousRot = transform.localRotation;
             transform.LookAt(_player.transform);
+            Quaternion TargetRot = transform.localRotation;
+            transform.localRotation = Quaternion.Slerp(PreviousRot, TargetRot, Time.deltaTime);
         }
+        else
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0,0,0), Time.deltaTime);
+        }
+        IsVisible = this.gameObject.GetComponent<MeshRenderer>().isVisible;
 
-	}
+    }
 }
