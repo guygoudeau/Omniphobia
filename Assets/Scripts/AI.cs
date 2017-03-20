1ada6acaf136e4ccb543eb8dfa6369102689a25a
Rory Vaughn <Rvaughn2525@gmail.com>
2017-03-20 16:59:07 -0500

    Spider AI continued

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -11,12 +11,24 @@ public class AI : MonoBehaviour {
     public Vector3 LastCheckpoint;
     float Distance;
     public float speed;
+    SortedList Waypoints;
     public Vector3 a;
     public Vector3 b;
     public Vector3 c;
     public Vector3 d;
+    public object OCurrent;
+    public Vector3 VCurrent;
+    int index;
 
-
+    private void Start()
+    {
+        Waypoints = new SortedList();
+        Waypoints.Add(0,a);
+        Waypoints.Add(1,b);
+        Waypoints.Add(2,c);
+        Waypoints.Add(3,d);
+        OCurrent = Waypoints[0];
+    }
     //public string Entity;
 
     // Gets called every frame.
@@ -37,83 +49,52 @@ public class AI : MonoBehaviour {
         }
         return AIPos;
     }
-    Vector3 RoundPos(Vector3 NewTarget, Vector3 CurrentPos, Vector3 Target)
+    bool RoundPos(Vector3 CurrentPos, Vector3 Target)
     {
         //gets the distance from This object to the Target location.
-        Distance = ((Target.x + Target.y + Target.z) / 3);
+        Distance = ((Mathf.Abs(Target.x) + Mathf.Abs(Target.y) + Mathf.Abs(Target.z)) / 3)
+            - (Mathf.Abs(CurrentPos.x) + Mathf.Abs(CurrentPos.y) + Mathf.Abs(CurrentPos.z)/3);
         //makes sure that distance is positive.
-        if (Distance <= 0)
-        {
-            Distance = -Distance;
-        }
         //checks if this object is close to its target.
-        if (Distance <= (offSetDis))
+        if (Distance <= (offSetDis /2))
         {
-            return NewTarget;
+            return true;
         }
         else
-        return CurrentPos;
+        return false;
     }
     Vector3 WayPoint(Vector3 Pos)
     {
-        if (LastCheckpoint == a)
-        {
-            if (Pos != b)
-            {
-                Pos = Pos + ((b - Pos) * Time.deltaTime * speed);
-                Pos = RoundPos(b, Pos, transform.position);
-            }
-            else
-            {
-                LastCheckpoint = b;
-            }
+        OCurrent = Waypoints[index];
 
-            return Pos;
-        }
-        if (LastCheckpoint == b)
+        if (OCurrent == Waypoints[0])
         {
-            if (Pos != c)
-            {
-                Pos = Pos + ((c - Pos) * Time.deltaTime * speed);
-                Pos = RoundPos(c, Pos, transform.position);
-            }
-            else
-            {
-                LastCheckpoint = c;
-            }
-
-            return Pos;
+            VCurrent = a;
         }
-        if (LastCheckpoint == c)
+        if (OCurrent == Waypoints[1])
         {
-            if (Pos != d)
-            {
-                Pos = Pos + ((d - Pos) * Time.deltaTime * speed);
-                Pos = RoundPos(d, Pos, transform.position);
-            }
-            else
-            {
-                LastCheckpoint = d;
-            }
-
-            return Pos;
+            VCurrent = b;
         }
-        if (LastCheckpoint == d)
+        if (OCurrent == Waypoints[2])
         {
-            if (Pos != a)
-            {
-                Pos = Pos + ((a - Pos) * Time.deltaTime * speed);
-                Pos = RoundPos(a, Pos, transform.position);
-            }
-            else
-            {
-                LastCheckpoint = a;
-            }
-
-            return Pos;
+            VCurrent = c;
         }
-        //Keeps LastChecpoint from being null
-        LastCheckpoint = a;
+        if (OCurrent == Waypoints[3])
+        {
+            VCurrent = d;
+        }
+
+        //changes the target when close enough
+        if (RoundPos(transform.position, VCurrent) == true)
+        {
+            index++;
+            if (index > 3)
+            {
+                index = 0;
+            }
+        }
+        //updates the position of the player.
+        Pos = Pos + ((VCurrent - Pos) * Time.deltaTime);
         return Pos;
     }
 }
