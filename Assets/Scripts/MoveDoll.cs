015a0b8c2f00cd77c51e4668ccb8916949244c7d
Logan Pletcher <lpletch95@hotmail.com>
2017-03-20 09:27:40 -0500

    Created a script that moves a Doll

---
+++ Assets/Scripts/MoveDoll.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveDoll : MonoBehaviour {
+
+    private bool _playerLooked = false;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(this.gameObject.GetComponentInChildren<MeshRenderer>().isVisible)
+        {
+            _playerLooked = true;
+        }
+        else if(_playerLooked)
+        {
+            float NewX = Random.Range(-2.5f, 2.5f);
+            float NewZ = Random.Range(-2.5f, 2.5f);
+            transform.position = new Vector3(transform.position.x + NewX, transform.position.y, transform.position.z + NewZ);
+            _playerLooked = false;
+        }
+	}
+}
