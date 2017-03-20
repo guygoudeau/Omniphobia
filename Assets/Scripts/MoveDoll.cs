278a493a1827e9514393422b349c2bcbd1a3388f
Logan Pletcher <lpletch95@hotmail.com>
2017-03-20 10:57:33 -0500

    Made new script and changed MoveDoll script

--- Assets/Scripts/MoveDoll.cs
+++ Assets/Scripts/MoveDoll.cs
@@ -1,12 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MoveDoll : MonoBehaviour {
 
     private bool _playerLooked = false;
+    private DollPosition _currentPos;
+    public List<DollPosition> _possiblePositions;
+    private float _posAmount = 0;
 
 	// Use this for initialization
 	void Start () {
+        foreach (DollPosition dp in FindObjectsOfType<DollPosition>())
+        { if (dp.transform.position == transform.position)
+            {
+                _currentPos = dp;
+                _currentPos.IsOccupied = true;
+                _possiblePositions.Add(dp);
+                _posAmount++;
+            }
+            else if (Mathf.Abs(dp.transform.position.magnitude - transform.position.magnitude) <= 1)
+            {
+                _possiblePositions.Add(dp);
+                _posAmount++;
+            }
+
+        }
     }
 	
 	// Update is called once per frame
@@ -17,9 +36,13 @@ public class MoveDoll : MonoBehaviour {
         }
         else if(_playerLooked)
         {
-            float NewX = Random.Range(-2.5f, 2.5f);
-            float NewZ = Random.Range(-2.5f, 2.5f);
-            transform.position = new Vector3(transform.position.x + NewX, transform.position.y, transform.position.z + NewZ);
+            int NewPos = (int)Random.Range(0, _posAmount);
+            while(_possiblePositions[NewPos] == _currentPos)
+                NewPos = (int)Random.Range(0, _posAmount);
+            _currentPos.IsOccupied = false;
+            _currentPos = _possiblePositions[NewPos];
+            _currentPos.IsOccupied = true;
+            transform.position = _currentPos.transform.position;
             _playerLooked = false;
         }
 	}
