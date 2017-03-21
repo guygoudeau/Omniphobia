6df87b11972a72a33f32f0f22856c3a214a78553
Rory Vaughn <Rvaughn2525@gmail.com>
2017-03-21 11:46:14 -0500

    Spider walk

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -111,7 +111,7 @@ public class AI : MonoBehaviour {
         //Pos = Pos + ((VCurrent - Pos) * Time.deltaTime);
         float distTraveled = (dest - Pos).magnitude;
         Vector3 displacement = (dest - Pos);
-        //self.rotation = Quaternion.Slerp(self.rotation, Quaternion.LookRotation(target.position - self.position), rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(VCurrent - transform.position), 3.0f * Time.deltaTime);
         Pos += displacement * Time.deltaTime * speed;
         return Pos;
     }
