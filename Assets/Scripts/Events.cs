51b453c759f51ca7a2178ae1ecb1a3e3a25d522d
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-04-03 18:12:02 -0500

    Redo the game manager

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -4,10 +4,27 @@ public static class Events
 {
     public static PlayerEvent PlayerWin = new PlayerEvent();
     public static PlayerEvent PlayerDeath = new PlayerEvent();
-    public static PlayerEvent PlayerReloadScene = new PlayerEvent();
-    public static PlayerEvent PlayerForceScene = new PlayerEvent();
+    public static PlayerEvent PlayerForceScene = new PlayerEvent();    
+
+    public static RoomEvent RoomCompleted = new RoomEvent();
+    public static RoomEvent RoomHeightSelected = new RoomEvent();
+    public static RoomEvent RoomSpiderSelected = new RoomEvent();
+    public static RoomEvent RoomDollSelected = new RoomEvent();
+    public static RoomEvent RoomClownSelected = new RoomEvent();
+
+    public static GameEvent GameRestarted = new GameEvent();
+    public static GameEvent GameStarted = new GameEvent();
 }
 
 public class PlayerEvent : UnityEvent
+{
+}
+
+
+public class RoomEvent : UnityEvent
+{
+}
+
+public class GameEvent : UnityEvent
 {
 }
