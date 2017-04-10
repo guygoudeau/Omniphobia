ffc588866faea4806fa8a2685a97befb652d3d5e
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-04-10 16:54:29 -0500

    updated player state systems

---
+++ Assets/Scripts/Player/PlayerStateSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerStateSystem : MonoBehaviour
+{
+    public bool isKillable;
+
+    private void Awake()
+    {
+        this.isKillable = false;        
+    }
+
+    void Start()
+    {
+        Events.PlayerCanDie.AddListener(CanBeKilled);
+        Events.PlayerCantDie.AddListener(CantBeKilled);
+        Events.PlayerEnteredKillBox.AddListener(EnteredKillVolume);
+    }	
+
+    void EnteredKillVolume()
+    {
+        if(this.isKillable)
+            Events.PlayerDeath.Invoke();
+    }
+
+    void CanBeKilled()
+    {
+        this.isKillable = true;
+        Debug.Log(this.isKillable + "IsKill");
+    }
+
+    void CantBeKilled()
+    {
+        this.isKillable = false;
+        Debug.Log(this.isKillable + "IsKill");
+    }
+}
