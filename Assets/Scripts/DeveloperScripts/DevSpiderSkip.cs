afd1d3d04d45dea7fe721d89d7b9a11350f21c72
Rory Vaughn <Rvaughn2525@gmail.com>
2017-04-11 16:26:43 -0500

    Cheat script and boss fight seperation

---
+++ Assets/Scripts/DeveloperScripts/DevSpiderSkip.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevSpiderSkip : MonoBehaviour {
+
+
+    GameObject Player;
+    public Transform Cheat;
+    public Transform Begin;
+    // Use this for initialization
+    void Start()
+    {
+        Player = FindObjectOfType<CharacterController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Four))
+        {
+            Player.transform.position = Cheat.position;
+            if (OVRInput.GetDown(OVRInput.Button.Two))
+            {
+                Player.transform.position = Begin.position;
+            }
+        }
+
+    }
+}
