e2b5b5a678a08eb15ab4a1f521516aacf912bbac
Rory Vaughn <Rvaughn2525@gmail.com>
2017-04-11 10:39:52 -0500

    Torch edits

--- Assets/Scripts/SpiderRoom/SpawnTorch.cs
+++ Assets/Scripts/SpiderRoom/SpawnTorch.cs
@@ -7,6 +7,7 @@ public class SpawnTorch : MonoBehaviour {
 
     public Transform Player;
     public bool Grabbed;
+    public bool spawnState;
 	// Use this for initialization
 	void Start () {
         Player = FindObjectOfType<CharacterController>().GetComponent<Transform>();
@@ -22,6 +23,7 @@ public class SpawnTorch : MonoBehaviour {
             transform.rotation = new Quaternion(0, 0, 0, 0);
             gameObject.GetComponent<Rigidbody>().isKinematic = true;
             gameObject.GetComponent<BoxCollider>().isTrigger = true;
+            spawnState = true;
             Grabbed = false;
         }
 
@@ -31,8 +33,20 @@ public class SpawnTorch : MonoBehaviour {
         }
      if (Grabbed == true)
         {
+            spawnState = false;
             gameObject.GetComponent<Rigidbody>().isKinematic = false;
             gameObject.GetComponent<BoxCollider>().isTrigger = false;
+            gameObject.GetComponent<Rigidbody>().useGravity = false;
+            transform.position = gameObject.GetComponent<OVRGrabbable>().snapOffset.position;
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+            if (spawnState == false)
+            {
+                gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                gameObject.GetComponent<BoxCollider>().isTrigger = false;
+            }
         }
        
 
