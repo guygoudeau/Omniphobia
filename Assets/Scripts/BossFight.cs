ad2e0558e16f60139aeb8a35ad3057603ab375c5
Rory Vaughn <Rvaughn2525@gmail.com>
2017-04-11 16:04:29 -0500

    Boss Spider created and functional

--- Assets/Scripts/BossFight.cs
+++ Assets/Scripts/BossFight.cs
@@ -5,6 +5,8 @@ public class BossFight : MonoBehaviour {
 
 
     GameObject Player;
+    public Transform Cheat;
+    public Transform Begin;
 	// Use this for initialization
 	void Start () {
         Player = FindObjectOfType<CharacterController>().gameObject ;
@@ -18,6 +20,14 @@ public class BossFight : MonoBehaviour {
     }
     // Update is called once per frame
     void Update () {
+        if (OVRInput.GetDown(OVRInput.Button.Four))
+            {
+            Player.transform.position = Cheat.position;
+            if (OVRInput.GetDown(OVRInput.Button.Two))
+            {
+                Player.transform.position = Begin.position;
+            }
+            }
 	
 	}
 }
