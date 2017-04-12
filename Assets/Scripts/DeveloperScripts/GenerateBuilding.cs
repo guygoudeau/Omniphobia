a253d5e7dbf3abe739fc4fa23b09a3eb97f89ce9
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-04-12 16:13:58 -0500

    We Built this city

---
+++ Assets/Scripts/DeveloperScripts/GenerateBuilding.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GenerateBuilding : MonoBehaviour
+{
+    public List<GameObject> BuildingPrefabs;
+    public List<GameObject> BuildingFoundation;
+    public List<List<GameObject>> Buildings;
+    public int numFloors;
+    
+    [ContextMenu("Gen Building")]
+    void GenBuilding()
+    {
+        Buildings = new List<List<GameObject>>();
+        foreach(var foundation in BuildingFoundation)
+        {
+            List<GameObject> Building = new List<GameObject>();
+            float lastYPos = 3;
+            for (int i = 0; i < numFloors; i++)
+            {
+                GameObject newFloor = Instantiate(BuildingPrefabs[Random.Range(0, BuildingPrefabs.Count)]);
+                newFloor.transform.parent = foundation.transform;
+                newFloor.transform.position = foundation.transform.position + new Vector3(0, lastYPos + 15, 0);
+                lastYPos = newFloor.transform.position.y;
+                Building.Add(newFloor);
+            }
+            Buildings.Add(Building);
+        }
+    }
+
+    [ContextMenu("Demo")]
+    void DemoCity()
+    {
+        if(Buildings != null)
+        {
+            for (int i = 0; i < Buildings.Count; i++)
+            {
+                for (int j = 0; j < Buildings[i].Count; j++)
+                {
+                    GameObject temp = Buildings[i][j];
+                    Buildings[i].Remove(temp);
+                    DestroyImmediate(temp);
+                }
+            }
+        }
+    }
+}
