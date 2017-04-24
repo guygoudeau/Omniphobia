975ff235cc9af848024113ce149c2781e2e6f37a
Rory Vaughn <Rvaughn2525@gmail.com>
2017-04-24 12:55:26 -0500

    attempt to fix end game problem

--- Assets/Scripts/SpiderRoom/BossFight.cs
+++ Assets/Scripts/SpiderRoom/BossFight.cs
@@ -6,7 +6,7 @@ public class BossFight : MonoBehaviour {
 
 
     GameObject Player;
-    private bool isEnabled = true;
+    [SerializeField] private bool isEnabled;
 
     // Use this for initialization
     void Start()
