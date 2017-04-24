449012ae11628444ca7135b53bc97dd7341c94ac
Rory Vaughn <Rvaughn2525@gmail.com>
2017-04-24 11:24:42 -0500

    end game function

--- Assets/Scripts/SpiderRoom/BossFight.cs
+++ Assets/Scripts/SpiderRoom/BossFight.cs
@@ -1,21 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
+using System.Linq;
 public class BossFight : MonoBehaviour {
 
 
     GameObject Player;
+    private bool isEnabled = true;
 
-	// Use this for initialization
-	void Start () {
-        Player = FindObjectOfType<CharacterController>().gameObject ;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Player = FindObjectOfType<CharacterController>().gameObject;
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other == Player.GetComponent<CharacterController>())
-        {
+        if (other.gameObject.tag != "Fire")
+            return;
+
+        isEnabled = true;
+
+        var braziers = FindObjectsOfType<BossFight>();
+        if (braziers.All(brazier => brazier.isEnabled))
             Events.RoomCompleted.Invoke();
-        }
+
+
     }
     // Update is called once per frame
     void Update()
