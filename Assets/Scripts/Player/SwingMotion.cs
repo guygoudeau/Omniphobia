a3bba1865bc81e9b8d015f76379ce2373a4ebaaa
Logan Pletcher <lpletch95@hotmail.com>
2017-04-24 16:41:01 -0500

    Revert "Scene Edit(WIP) and Script Edits"
    This reverts commit 973da0a1aea45a7e4142e44b5698c60b3988d689.

--- Assets/Scripts/Player/SwingMotion.cs
+++ Assets/Scripts/Player/SwingMotion.cs
@@ -11,16 +11,9 @@ public class SwingMotion : MonoBehaviour
         lTouchVelocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LTouch).magnitude;
         rTouchVelocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch).magnitude;
 
-        var rigidbody = GetComponent<Rigidbody>();
-        if (rigidbody == null)
-        {
-            Debug.Log("The GameObject:" + name + " needs a rigidbody");
-            return;
-        }
-
         if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger) && OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
         {
-            rigidbody.velocity = transform.forward * (lTouchVelocity + rTouchVelocity);
+            transform.position += transform.forward * (lTouchVelocity + rTouchVelocity) * Time.deltaTime;
         }
     }
 }
